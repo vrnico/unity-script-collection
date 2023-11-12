b3e8bde7db4ffd6487044945b054ccc8fb28c2dc
vrnico <nico.daunt@gmail.com>
2023-11-12 15:01:00 -0800

    Create ToolTemplateSDK.cs

---
+++ ToolTemplateSDK.cs
@@ -0,0 +1,17 @@
+#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+
+public class YourEditorTool : MonoBehaviour
+{
+    // Your tool's code here
+
+    // Example function
+    public void ExampleFunction()
+    {
+        // Function implementation
+    }
+}
+
+// You can also include custom Editor windows, inspectors, or other editor-only functionality here
+#endif
